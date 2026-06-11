accfd8c83990bc50740980a7aa48ee018f7755b4
developerthierry <developerthierry@gmail.com>
2026-06-11 21:28:24 +0000

    Create our React form structure

---
+++ src/components/BaristaForm.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const BaristaForm = () => {
+  const onCheckAnswer = () => {};
+  const onNewDrink = () => {};
+  return (
+    <div>
+      <h2>Hi, I'd like to order a:</h2>
+
+      <form action=""></form>
+      <button className="button submit" onClick={onCheckAnswer}>
+        Check Answer
+      </button>
+      <button className="button submit" onClick={onNewDrink}>
+        New Drink
+      </button>
+    </div>
+  );
+};
+
+export default BaristaForm;
